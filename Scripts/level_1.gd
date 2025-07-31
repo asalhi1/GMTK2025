4444c18653111cf5cebee3cd8934c2d93b85ab5f
Adam Salhi <adamtherookie@proton.me>
2025-07-31 20:32:12 +0100

    updated level 1

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -1,16 +1,54 @@
-extends Node2D
+extends Control
 
-@onready var player := $Player
-@onready var progress_bar := $ProgressBar
+@export var player : Node2D
+@export var progress_bar : ProgressBar
 
-@export var start_x : float = 280.0
-@export var end_x : float = 800.0
+var start_x
+var end_x
+var speed = 250.0 
+var velocity = 0.0
+var friction = 5
+var acceleration = 5
 
-func _ready() -> void:
-	player.global_position.x = 280.0
+var is_fake_loading = false
+var fake_progress = 0
 
-func _process(delta: float) -> void:
-	var player_x : float = clamp(player.global_position.x, start_x, end_x)
+func _ready():
+	await get_tree().process_frame
+	var bar_rect = progress_bar.get_global_rect()
+	start_x = bar_rect.position.x
+	end_x = bar_rect.position.x + bar_rect.size.x
 
-	var t : float = (player_x - start_x) / (end_x - start_x)
-	progress_bar.value = lerp(progress_bar.min_value, progress_bar.max_value, t)
+	print(str(start_x) + " " + str(end_x))
+	
+	fake_progress = 0.0
+	progress_bar.value = 0.0
+	is_fake_loading = true
+	
+	player.position.x = start_x
+	player.position.y = bar_rect.position.y - 100
+
+func _process(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction != 0:
+		velocity = lerp(velocity, direction * speed, acceleration * delta)
+	else:
+		velocity = lerp(velocity, 0.0, friction * delta)
+	
+	if is_fake_loading:
+		fake_progress += 7 * delta 
+		if fake_progress >= 21.0:
+			fake_progress = 21.0
+			is_fake_loading = false  
+		progress_bar.value = fake_progress
+		player.position.x = start_x + (fake_progress / 100) * (end_x - start_x)
+	else:
+		player.position.x += velocity * delta
+		player.position.x = clamp(player.position.x, start_x, end_x)
+		
+		var progress = (player.position.x - start_x) / (end_x - start_x)
+		progress = clamp(progress, 0.0, 1.0)
+		progress_bar.value = progress * 100.0
+	
+	if progress_bar.value >= 95:
+		TransitionManager.transition_to("res://Levels/level2.tscn")
